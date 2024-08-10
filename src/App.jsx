d9e5e194cf75e3440592bf62c8ec96e0624c9033
KevinKasozi <Kevinkasozi@gmail.com>
2024-08-10 23:20:14 +0000

    Add small changes to get the form lambda to trigger

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ const App = () => (
         <Route path="/charity/donate" element={<Donate />} />
         <Route path="/" element={<HospitalHome />} />
         <Route path="*" element={<NotFound />} />
+        
       </Routes>
     </Router>
   </div>
