f17ef9216eeb532f477d859270d617c2b9ee534e
KevinKasozi <Kevinkasozi@gmail.com>
2024-08-11 17:54:04 +0000

    Set routing the correct address

--- netlify/functions/submitForm.js
+++ netlify/functions/submitForm.js
@@ -1,8 +1,30 @@
+const nodemailer = require('nodemailer');
+
 exports.handler = async (event, context) => {
-  console.log("Function invoked");
   try {
     const data = JSON.parse(event.body);
     console.log('Form data received:', data);
+
+    // Create a transporter
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER, // Your email address
+        pass: process.env.EMAIL_PASS, // Your email password
+      },
+    });
+
+    // Email options
+    const mailOptions = {
+      from: process.env.EMAIL_USER, // Sender address
+      to: 'nsobanimedical.center@gmail.com', // Recipient address
+      subject: `New message from ${data.name}`, // Subject line
+      text: `You have a new message from ${data.name} (${data.email}):\n\n${data.message}`, // Plain text body
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Form submission successful!' }),
