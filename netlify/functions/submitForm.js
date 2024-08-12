a7eb99dcd1c333bf4293ca6f78c80bece5add6a3
KevinKasozi <Kevinkasozi@gmail.com>
2024-08-12 22:08:52 +0000

    fix ports

--- netlify/functions/submitForm.js
+++ netlify/functions/submitForm.js
@@ -1,16 +1,16 @@
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 exports.handler = async (event, context) => {
-  console.log("Function invoked");
   try {
     const data = JSON.parse(event.body);
     console.log('Form data received:', data);
 
     // Create a transporter for IONOS
     const transporter = nodemailer.createTransport({
-      host: 'smtp.ionos.com',
-      port: 465, // You can also use 465 for secure connection, but make sure to adjust the `secure` field
-      secure: false, // Set to true if you are using port 465
+      host: 'smtp.ionos.co.uk',
+      port: 587, // Port 587 with TLS
+      secure: false, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER, // Your IONOS email address
         pass: process.env.EMAIL_PASS, // Your IONOS email password
@@ -20,17 +20,13 @@ exports.handler = async (event, context) => {
     // Email options
     const mailOptions = {
       from: process.env.EMAIL_USER, // Sender address
-      to: 'contact@nsobanifoundationclinic.ch', // Recipient address
+      to: 'nsobanimedical.center@gmail.com', // Recipient address
       subject: `New message from ${data.name}`, // Subject line
       text: `You have a new message from ${data.name} (${data.email}):\n\n${data.message}`, // Plain text body
     };
 
-    // Log the mail options to ensure they are correct
-    console.log('Mail options:', mailOptions);
-
     // Send the email
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent:', info.response);
+    await transporter.sendMail(mailOptions);
 
     return {
       statusCode: 200,
